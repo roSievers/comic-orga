ad338c0c2ffa2c6ab155134b308a1485e6710f58
Rolf Sievers <rolf.sievers@posteo.de>
2017-09-14 14:12:10 +0200

    Draft sanitize module

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-from sanitize import sanitize
+from sanitize import sanitize, maybe, mapping
 import time
 
 class Database:
@@ -8,35 +8,38 @@ class Database:
         self.conn = sqlite3.connect(filename)
         self.c = self.conn.cursor()
 
-    def comics(self, user=None):
-        if user is None:
+    @sanitize(None, maybe(int))
+    def comics(self, user_id=None):
+        if user_id is None:
             comics = self.c.execute("select * from comics")
         else:
             query = f"""select following.comic, comics.name, comics.link from following
                             join comics on following.comic = comics.id
-                            where following.user={user}"""
+                            where following.user={user_id}"""
             comics = self.c.execute(query)
         for comic in comics:
             print(comic)
 
-    @sanitize
+    @sanitize(None, str, str)
     def new_comic(self, name, link):
         query = f"insert into comics (name, link) values ('{name}', '{link}')"
         self.c.execute(query)
         self.conn.commit()
 
-    @sanitize
+    @sanitize(None, str)
     def new_user(self, name):
         # TODO: Generate password
         query = f"insert into users (name) values ('{name}')"
         self.c.execute(query)
         self.conn.commit()
 
+    @sanitize(None, int, int)
     def subscribe(self, user_id, comic_id):
         query = f"insert into following (user, comic) values ('{user_id}', '{comic_id}')"
         self.c.execute(query)
         self.conn.commit()
 
+    @sanitize(None, int, str, str, mapping(str, str))
     def new_page(self, comic_id, title, link, **data):
         timestamp = int(time.time())
         query = f"""insert into pages (comic, title, link, timestamp)
@@ -48,6 +51,7 @@ class Database:
             print(query)
             self.c.execute(query)
 
+    @sanitize(None, int, int)
     def unread(self, comic_id, timestamp):
         query = f"""select id, title, link, timestamp from pages
                         where (timestamp >= '{timestamp}')
@@ -56,6 +60,7 @@ class Database:
         for page in pages:
             print(page)
 
+    @sanitize(None, int)
     def page(self, page_id):
         query = f"""select (comic, title, link) from pages where id={page_id}"""
 
--- src/sanitize.py
+++ src/sanitize.py
@@ -1,6 +1,14 @@
 
-def sanitize(fn):
-    def inner(*args, **aargs):
-        # TODO: Actually sanitize the data
-        return fn(*args, **aargs)
-    return inner
+def sanitize(*args, **aargs):
+    def outer(fn):
+        def inner(*args, **aargs):
+            # TODO: Actually sanitize the data
+            return fn(*args, **aargs)
+        return inner
+    return outer
+
+def maybe(type):
+    return type
+
+def mapping(key_type, value_type):
+    return value_type
