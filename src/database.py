5350520f21bd12a28b4bf74a387ea0d7e931fe98
Alrik Birgelbaum <Adeptus.Alricus@gmail.com>
2017-10-09 11:43:27 +0200

    insanity update: sqlite3 already has its own sanitizer. Deleted sanitize.py. Now using: integrated sanitizer

--- src/database.py
+++ src/database.py
@@ -25,63 +25,56 @@ class Database:
         del data["title"]
         self.new_page(comic_id, title, comic_url, **data)
 
-    @sanitize(None, maybe(int))
     def comics(self, user_id=None):
         if user_id is None:
             comics = self.c.execute("select * from comics")
         else:
             query = \
-            f"""select following.comic, comics.name, comics.link from following
+            """select following.comic, comics.name, comics.link from following
                join comics on following.comic = comics.id
-               where following.user={user_id}"""
+               where following.user= ? """
 
-            comics = self.c.execute(query)
+            comics = self.c.execute(query,user_id)
         for comic in comics:
             print(comic)
 
-    @sanitize(None, str, str)
     def new_comic(self, name, link):
-        query = f"insert into comics (name, link) values ('{name}', '{link}')"
-        self.c.execute(query)
+        query = "insert into comics (name, link) values (?, ?)"
+        self.c.execute(query,(name,link))
         self.conn.commit()
 
-    @sanitize(None, str)
     def new_user(self, name):
         # TODO: Generate password
-        query = f"insert into users (name) values ('{name}')"
-        self.c.execute(query)
+        query = "insert into users (name) values ( ? ) "
+        self.c.execute(query,name)
         self.conn.commit()
 
-    @sanitize(None, int, int)
     def subscribe(self, user_id, comic_id):
-        query = f"""insert into following (user, comic)
-                    values ('{user_id}', '{comic_id}')"""
-        self.c.execute(query)
+        query = """insert into following (user, comic)
+                   values (?, ?)"""
+        self.c.execute(query,(user_id,comic_id))
         self.conn.commit()
 
-    @sanitize(None, int, str, str, mapping(str, str))
     def new_page(self, comic_id, title, link, **data):
         timestamp = int(time.time())
-        query = \
-        f"""insert into pages (comic, title, link, timestamp)
-           values ('{comic_id}', '{title}', '{link}', '{timestamp}')"""
+        query = """insert into pages (comic, title, link, timestamp)
+                   values (?, ?, ?, ?)"""
 
-        page_id = self.c.execute(query).lastrowid
+        page_id = self.c.execute(query,(comic_id, title, link, timestamp)).lastrowid
         for key, value in data.items():
-            query = f"""insert into data (page, key, value)
-                            values ('{page_id}', '{key}', '{value}')"""
-            print(query)
+            query = """insert into data (page, key, value)
+                            values (?, ?, ?)"""
+            print(query,(page_id, key, value))
             self.c.execute(query)
 
-    @sanitize(None, int, int)
     def unread(self, comic_id, timestamp):
-        query = f"""select id, title, link, timestamp from pages
-                        where (timestamp >= '{timestamp}')
-                          and (comic = '{comic_id}')"""
-        pages = self.c.execute(query)
+        query = """select id, title, link, timestamp from pages
+                        where (timestamp >= ?)
+                          and (comic = ?)"""
+        pages = self.c.execute(query,(timestamp,comic_id))
         for page in pages:
             print(page)
 
-    @sanitize(None, int)
     def page(self, page_id):
-        query = f"""select (comic, title, link) from pages where id={page_id}"""
+        query = f"""select (comic, title, link) from pages where id= ?"""
+        self.c.execute(query,comic_id)
